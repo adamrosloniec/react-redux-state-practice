72a9f0c06b75d95074f5fe5430aee8fa2d86bdbd
Adam Rosłoniec <adam@rosloniec.pl>
2019-04-14 22:55:44 +1000

    publish

---
+++ build/precache-manifest.7a5c90b95957d4bb0012d030f6dde99a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed302da6d1594d562592",
+    "url": "/react-redux-state-practice/static/css/main.2cd85434.chunk.css"
+  },
+  {
+    "revision": "ed302da6d1594d562592",
+    "url": "/react-redux-state-practice/static/js/main.0e3eaee1.chunk.js"
+  },
+  {
+    "revision": "7102921ebc2eb7dd75eb",
+    "url": "/react-redux-state-practice/static/js/runtime~main.07f99d5a.js"
+  },
+  {
+    "revision": "7466508ae6c36a7eba50",
+    "url": "/react-redux-state-practice/static/js/2.9bafa7f5.chunk.js"
+  },
+  {
+    "revision": "9c7efbe25b67e953819c2096aebe9572",
+    "url": "/react-redux-state-practice/index.html"
+  }
+];
