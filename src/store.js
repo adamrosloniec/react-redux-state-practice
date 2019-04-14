6d8447a829329608785f12b7e6a1ae1010c437da
Adam Rosłoniec <adam@rosloniec.pl>
2019-04-14 22:38:58 +1000

    Updates

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducers'
-import { movieActions } from './app/movies/duck'
-
-const store = createStore(rootReducer, composeWithDevTools())
-store.dispatch(movieActions.add('Rambo X'))
-
-export default store
